42e81fc3ee608a0dd02cae16b9f2dadeb12853e0
Alexsander Albino <alexsander.santos.contato@gmail.com>
2025-06-06 10:47:17 -0300

    first commit

---
+++ ideias.py
@@ -0,0 +1,18 @@
+'''
+Precisamos criar um sistema de vendas funcional usando (talvez) apenas o python.
+
+Para isso vamos tentar para a parte de comprar, o carrinho, criar uma lista que o usuário vá adicionando Dicionários de cada produto, onde vai conter nome, descrição e preço. E nessa lista/dicionário (não sei ainda) ter a função para calcular o preço total da compra com os valores contidos dentro dos dicionários dos produtos, e para luxar um pouco podemos fazer com que apareça as opções de parcelamento... com juros crescentes a partir de 3x, para agregar mais no código. Além de uma função para remover produtos
+
+Na parte onde ficarão os produtos faremos:
+Um contador da quantidade de produtos usando um cont ** (opcional)
+Uma lista com todos os produtos, sendo cada produto um dicionário
+A opção de adicionar mais de um produto de uma vez (Adicionar 10x copos, por exemplo, para o usuário não precisar adicionar 10x)
+Imagens do produto talvez? (Com o uso do streamlit)
+
+USAR O STREAMLIT PARA TER UMA INTERFACE (precisamos estudar sobre, mas ficaria bem legal)
+
+Abrir um repositório no github pra todo mundo acompanhar
+
+Até dia 10 para entregar -> 14 dias para fazer. Dá?
+
+'''
